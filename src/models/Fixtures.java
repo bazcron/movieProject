caa66dbf25bb64bb8c6a18b9a046510992f638d1
Conor <Conor@Acer-Aspire>
2017-10-28 16:13:03 +0100

    updates

---
+++ src/models/Fixtures.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Fixtures
+{
+  public static User[] users =
+  {
+    new User ("marge", "simpson", "marge@simpson.com", 1, "this", "secret"),
+    new User ("lisa",  "simpson", "lisa@simpson.com",  2, "this",  "secret"),
+    new User ("bart",  "simpson", "bart@simpson.com",  3, "this", "secret"),
+    new User ("maggie","simpson", "maggie@simpson.com", 4, "this", "secret")
+  };
+}
