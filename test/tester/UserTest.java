1a264fb0015fac283be1d639ca2619db9158ceb1
Conor <Conor@Acer-Aspire>
2017-10-28 16:11:45 +0100

    updat

---
+++ test/tester/UserTest.java
@@ -0,0 +1,44 @@
+package tester;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Test;
+
+import models.User;
+
+import static models.Fixtures.users;
+
+public class UserTest
+{
+  User homer = new User ("6","homer", "simpson", 5, "m", "plumber");
+
+  @Test
+  public void testCreate()
+  {
+	  assertEquals ("6", homer.id);
+    assertEquals ("homer", homer.firstName);
+    assertEquals ("simpson", homer.lastName);
+    assertEquals (5,   homer.age);   
+    assertEquals ("m", homer.gender);
+    assertEquals ("plumber", homer.occupation);
+  }
+/*
+  @Test
+  public void testIds()
+  {
+    Set<Long> ids = new HashSet<>();
+    for (User user : users)
+    {
+      ids.add(user.id);
+    }
+    assertEquals (users.length, ids.size());
+  }
+*/
+  @Test
+  public void testToString()
+  {
+    assertEquals ("User{" + ", 6,homer, simpson, 5, m,plumber}", homer.toString());
+  }
+}
